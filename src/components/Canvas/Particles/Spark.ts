43bb120ed19c7c638f3b3158f27b808d64852811
Limbo <sounds.of.limbo@gmail.com>
2022-04-27 01:07:15 +0300

    color transition POC

--- src/components/Canvas/Particles/Spark.ts
+++ src/components/Canvas/Particles/Spark.ts
@@ -7,10 +7,15 @@ import { FRAME_TIMING } from "consts/canvas"
 import { unsafeRandomInt } from "utils/number"
 import { FREE_FALL_ACC_PER_FRAME } from "consts/physics"
 import { pixelizeSpeed } from "utils/physics"
+import Color from "entities/canvas/Color"
 
 export default
 class SparkParticle
 implements IDrawable {
+	private frameMultiplier
+		: number
+		= FRAME_TIMING / this.lifetime
+
 	private speed
 		= {...this.initialSpeed}
 
@@ -19,7 +24,20 @@ implements IDrawable {
 
 	private sizeReduction
 		: number
-		= this.size / this.lifetime * FRAME_TIMING
+		= this.size * this.frameMultiplier
+
+	private color
+		= new Color(this.colors.from.rgb)
+
+	private colorDifference
+		= this.colors.from.getColorDifference(this.colors.to)
+
+	private colorChange
+		= {
+			r: this.colorDifference.r * this.frameMultiplier,
+			g: this.colorDifference.g * this.frameMultiplier,
+			b: this.colorDifference.b * this.frameMultiplier,
+		}
 
 	constructor(
 		private readonly initialPosition: Coords2D,
@@ -28,9 +46,18 @@ implements IDrawable {
 		private readonly initialSpeed: Speed2D = {
 			vx: unsafeRandomInt(3, -3),
 			vy: unsafeRandomInt(4, 6),
-		}
+		},
+		private readonly colors: {
+			from: Color
+			to: Color
+		} = {
+			from: new Color("#f00"),
+			to: new Color("#ff0")
+		},
 	) {
-
+		console.log(this.colorChange)
+		console.log(this.colors.from.rgb)
+		console.log(this.colors.to.rgb)
 	}
 
 	draw = (
@@ -40,8 +67,11 @@ implements IDrawable {
 			return
 
 		const halfSize = this.size / 2
+		const { rgb } = this.color
 
-		context.fillStyle = `rgba(255, 255, 0, 1)`
+		console.log(`rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 1)`)
+
+		context.fillStyle = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 1)`
 		context.fillRect(
 			this.position.x - halfSize,
 			this.position.y - halfSize,
@@ -54,6 +84,8 @@ implements IDrawable {
 		this.size -= this.sizeReduction
 		this.position.x += pixelSpeed.vx / 1000 * FRAME_TIMING
 		this.position.y -= pixelSpeed.vy / 1000 * FRAME_TIMING
+
+		this.color.addColor(this.colorChange)
 
 		this.speed.vy -= FREE_FALL_ACC_PER_FRAME
 	}
